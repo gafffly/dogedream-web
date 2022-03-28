c2bfb90065ed20b9132c91de596de7a8dfd051cd
givingwu <givingwu@gmail.com>
2022-03-29 00:01:51 +0800

    feat: Integrate NProgress Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import 'windi.css'
+// @ts-ignore
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { Router } from "next/router"
+
+NProgress.configure({ showSpinner: false })
+
+Router.events.on("routeChangeStart", NProgress.start)
+Router.events.on("routeChangeComplete", NProgress.done)
+Router.events.on("routeChangeError", NProgress.done)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
